74dbfba0ffbff4d1ee7e16976cbcfcf001e4367c
tara z. manicsic <tzmanics@gmail.com>
2017-11-06 23:00:40 -0500

    adding kendo ui components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { DatePicker } from '@progress/kendo-dateinputs-react-wrapper';
 import '@progress/kendo-theme-material/dist/all.css';  
-
+import '@progress/kendo-ui';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1> What date would you like? </h1>
+        <DatePicker
+          value="pick a date"
+          min={ new Date(2017, 11, 1) }
+          max={ new Date(2018, 11, 1) }
+          format={ "yyyy/MM/dd" }
+        />
       </div>
     );
   }
